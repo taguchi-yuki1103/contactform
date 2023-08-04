26c36ab6f1872476f37f476b3fbc330e42318c6f
taguchi-yuki1103 <yuki20110723@gmail.com>
2023-08-04 15:49:10 +0900

    contactform

---
+++ insert.php
@@ -0,0 +1,34 @@
+<?php
+mb_internal_encoding("utf8");
+
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+
+
+$pdo->exec("insert into contactform(name,mail,age,comments)
+values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
+
+?>
+
+
+<!doctype HTML>
+<html lang="Ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>お問い合わせフォームを作る</title>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+
+<body>
+
+    <h1>お問い合わせフォーム</h1>
+
+    <div class="confirm">
+        <p>お問い合わせ有り難うございました。
+            <br>3営業日以内の担当者よりご連絡差し上げます。
+        </p>
+    </div>
+
+
+</body>
+</html>
